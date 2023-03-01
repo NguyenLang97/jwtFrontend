ad2c5c9c534458101fe634137066d914083f1b65
NguyenLang97 <nguyenvanlang1997@gmail.com>
2023-03-01 19:42:56 +0700

    Bearer tokken

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -51,6 +51,7 @@ const Login = (props) => {
         token,
         account: { groupWithRoles, email, username }
       };
+      localStorage.setItem('jwt', token);
       loginContext(data);
       history.push('/users');
     }
